df525eefcafc88df6afe7a31f3bc54cf04a2e26d
KHELILI <adrian.khelili@etu.univ-lyon1.fr>
2019-02-10 21:52:47 +0100

    test

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -2,8 +2,18 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 import csv
 
-with open("../datas/train.tsv", 'r') as G:
-    print(G.head(10))
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-#with open("../datas/test.tsv", 'r') as D: 
-#    print(D.read)
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+print(os.listdir("../datas"))
+import gc
+pd.options.display.max_colwidth=100
+pd.set_option('display.max_colwidth',100)
+
+train=pd.read_csv('../datas/train.tsv',sep='\t')
+print(train.shape)
+print(train.head(100))
