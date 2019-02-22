3768f474fb9f064acdbcf8b6c7af1e291e83297e
KHELILI <adrian.khelili@etu.univ-lyon1.fr>
2019-02-22 18:12:17 +0100

    g

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,50 +1,38 @@
-from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd 
 
-import csv
+train = pd.DataFrame()
+train = pd.read_csv('../datas/train.tsv',delimiter='\t',encoding='utf-8')
+test = pd.read_csv('../datas/test.tsv', delimiter='\t', encoding='utf-8') 
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+all_corpus   = list(train['Phrase'].values) + list(test['Phrase'].values)
+train_phrases  = list(train['Phrase'].values) 
+test_phrases   = list(test['Phrase'].values)
+X_train_target_binary = pd.get_dummies(train['Sentiment'])
 
-import matplotlib.pyplot as plt #for vizualization of data
-from tqdm import tqdm #for progress information
-tqdm.pandas()
+from keras.preprocessing import text, sequence
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
-#Packages for Preprocessing Data and building the embedding layer
-from sklearn.model_selection import train_test_split #for splitting the data into train and into test set
-from keras.preprocessing.text import Tokenizer #for preprocessing the data
-from keras.preprocessing.sequence import pad_sequences #for preprocessing the data
 
-#Packages for building the Neural Network: Keras Packages.
-import keras.backend as K #to use math functions like "keras.backend.sum"
-from keras.models import Model #to build the Model
-from keras.layers import Dense, Input, Dropout, LSTM, Activation,Bidirectional, CuDNNGRU # the layers we will use
-from keras.layers.embeddings import Embedding #to create an Embedding, more in chapter 3
-from keras.engine.topology import Layer #to create the attention layer
-from keras import initializers, regularizers, constraints #used in attention layer
+vocab_size = 20000  # based on words in the entire corpus
+max_len = 60
+tokenizer = Tokenizer(num_words=vocab_size, lower=True, filters='\n\t')
+tokenizer.fit_on_texts(all_corpus)
+encoded_train_phrases = tokenizer.texts_to_sequences(train_phrases)
+encoded_test_phrases = tokenizer.texts_to_sequences(test_phrases)
 
-from keras.preprocessing import sequence #scheint bei mir dafür zu sorgen, dass Layers richtig geordnet sind. Keine Ahnung warum.
+X_train_words = sequence.pad_sequences(encoded_train_phrases, maxlen=max_len,  padding='post')
+X_test_words = sequence.pad_sequences(encoded_test_phrases, maxlen=max_len,  padding='post')
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+import io
 
-import os
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
 
-print(os.listdir("../datas"))
-#else print(os.listdir("./datas"))
-import gc
-#pd.options.display.max_colwidth=100
-#pd.set_option('display.max_colwidth',100)
-
-#train=pd.read_csv('../datas/train.tsv')
-#print(train.shape)
-#print(train.head(100))
-
-embeddings_index = {}    #creates empty list
-glove = open('../datas/train.tsv') #opens the test document for reading
-for line in tqdm(glove): #for every line in this text do the following        (tqdm: and show the progress)
-    values = line.split(" ")  #splits the string every time there is a space into seperate strings
-    word = values[0] #the first string in this text file is always the word
-    #coefs = np.asarray(values[1:], dtype=np.float32) # the following strings are the "explanation"
-    print(word)
-    embeddings_index[word] = 2 #the list is now filled with entries consisting of the word and the respective "explanations" (word vectors)
-glove.close() #closes the file such that is not possible to read it anymore
+#tab = load_vectors("../datas/wiki-news-300d-1M.vec")
