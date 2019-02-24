acf48a3526f001a2cc084584f20f5f7e81349fac
KHELILI <adrian.khelili@etu.univ-lyon1.fr>
2019-02-24 16:46:55 +0100

    te

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -11,6 +11,9 @@ X_train, X_test, y_train, y_test = train_test_split(data.drop("Sentiment", axis=
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
+X_train=X_train['Phrase']
+X_test=X_test['Phrase']
+
 tokenizer= Tokenizer()
 total_sentences =  data['Phrase']
 tokenizer.fit_on_texts(total_sentences)
@@ -32,7 +35,7 @@ X_test_norm = pad_sequences(X_test_tok, maxlen= max_length, padding = 'post')
 #Implémentation du modèle LSTM 
 
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, GRU
+from keras.layers import Dense, Embedding, LSTM, GRU, Dropout
 from keras.layers.embeddings import Embedding
 
 Embedding_Dim = 100
@@ -40,10 +43,15 @@ print('Building model...')
 
 model = Sequential()
 model.add(Embedding(total_size,Embedding_Dim,input_length= max_length))
+model.add(Dropout(0.3))
 model.add(GRU(units=32,dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation = 'sigmoid'))
+model.summary()
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(X_train.shape)
 print("train ... ")
-#model.fit(X_train_norm, y_train,batch_size=128,epochs=25,validation_data=(X_test_norm,y_test), verbose=2)
+model.fit(X_train_norm, y_train,batch_size=128,epochs=8,validation_data=(X_test_norm,y_test), verbose=2)
+model.predict("zeb")
+#Ce type de model me retourne une accuracy d'environ 0.17, ce qui est insuffisant
+# Nous allons donc essayer 
+
