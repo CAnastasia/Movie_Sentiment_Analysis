457cee0188f34f0a74a11faa6e6264fdf6a21141
mannk <kostia.mannevy@gmail.com>
2019-03-10 00:11:59 +0100

    Demo website update

---
+++ demo_website/helpers/random_sentence.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from random import randint
+
+def get_random_sentence():
+    train_path = '../datas/train.tsv'
+    train_data = pd.read_csv(train_path, sep="\t")
+    rnd_id = randint(1, train_data['SentenceId'].max())
+    random_sample = train_data.loc[train_data['SentenceId'] == rnd_id].iloc[0].loc['Phrase']
+    return random_sample;
