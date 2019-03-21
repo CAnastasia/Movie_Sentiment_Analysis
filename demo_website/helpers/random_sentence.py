ccd1607fbb1f672c8bb6b0e94d999ac8e1263f75
KHELILI <adrian.khelili@etu.univ-lyon1.fr>
2019-03-21 20:00:18 +0100

    ge

--- demo_website/helpers/random_sentence.py
+++ demo_website/helpers/random_sentence.py
@@ -6,4 +6,4 @@ def get_random_sentence():
     train_data = pd.read_csv(train_path, sep="\t")
     rnd_id = randint(1, train_data['SentenceId'].max())
     random_sample = train_data.loc[train_data['SentenceId'] == rnd_id].iloc[0].loc['Phrase']
-    return random_sample;
+    return random_sample
